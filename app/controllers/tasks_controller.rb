8ea629b6538103607266e495e823fc4ab36fdc77
Dominique Zimmermann <dzim@gmx.net>
2020-11-10 23:05:56 +0100

    Day 27 - Exercise 2 - Rails task manager - clean code

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,5 @@
 class TasksController < ApplicationController
-  before_action :set_restaurant, only: [ :show ]
-  # :edit, :update, :destroy
+  before_action :set_task, only: [ :show, :edit, :update, :destroy ]
 
   def index
     @tasks = Task.all
@@ -9,11 +8,40 @@ class TasksController < ApplicationController
   def show
   end
 
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    if @task.save
+      redirect_to tasks_path
+    else
+      render "new"
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    @task.update(task_params)
+    redirect_to task_path(@task)
+  end
+
+  def destroy
+    @task.destroy
+    redirect_to tasks_path
+  end
 
   private
 
-  def set_restaurant
+  def set_task
     @task = Task.find(params[:id])
   end
-  
+
+  def task_params
+    params.require(:task).permit(:title, :details)
+  end
+
 end
